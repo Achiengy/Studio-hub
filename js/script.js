ffb9e916e67f970172802ab934089236105d1188
Achiengy <hope.pete@student.moringaschool.com>
2022-03-14 01:14:35 +0300

    st:portfolio_hover-This commit will add a hover effect on the portfolio

--- js/script.js
+++ js/script.js
@@ -1,24 +1,97 @@
-
-
 $(document).ready(function(){
- $("#design").click(function(){
-     $("#design").hide();
-     $("#text1").hide();
-    $("#title1").toggle();
-    $("#hidden1").toggle();
- });
 
- $("#development").click(function(){
-    $("#development").hide();
-    $("#text2").hide();
-    $("#title2").toggle();
-    $("#hidden2").toggle();
- });
 
- $("#product").click(function(){
-    $("#product").hide();
-    $("#text3").hide();
-    $("#title3").toggle();
-    $("#hidden3").toggle();
- });
+$("#black").hover(
+   function () {
+     $(this).find(".word1").fadeIn(200);
+   },
+   function () {
+     $(this).find(".word1").fadeOut(100);
+   }
+ );
+ 
+ $("#ontario").hover(
+   function () {
+     $(this).find(".word2").fadeIn(200);
+   },
+   function () {
+     $(this).find(".word2").fadeOut(100);
+   }
+ );
+ 
+ $("#golden").hover(
+   function () {
+     $(this).find(".word3").fadeIn(200);
+   },
+   function () {
+     $(this).find(".word3").fadeOut(100);
+   }
+ );
+ 
+ $("#carrey").hover(
+   function () {
+     $(this).find(".word4").fadeIn(200);
+   },
+   function () {
+     $(this).find(".word4").fadeOut(100);
+   }
+ );
+ 
+ $("#enjoy").hover(
+   function () {
+     $(this).find(".word5").fadeIn(200);
+   },
+   function () {
+     $(this).find(".word5").fadeOut(100);
+   }
+ );
+ 
+ $("#button").hover(
+   function () {
+     $(this).find(".word6").fadeIn(200);
+   },
+   function () {
+     $(this).find(".word6").fadeOut(100);
+   }
+ );
+ 
+ $("#burned").hover(
+   function () {
+     $(this).find(".word7").fadeIn(200);
+   },
+   function () {
+     $(this).find(".word7").fadeOut(100);
+   }
+ );
+ 
+ $("#bailey").hover(
+   function () {
+     $(this).find(".word8").fadeIn(200);
+   },
+   function () {
+     $(this).find(".word8").fadeOut(100);
+   }
+ );
+
 });
+
+
+
+// WHAT-WE-DO TOGGLE EFFECT ON ICONS
+$(document).ready(function () {
+   $(".design").click(function () {
+     $("#design").toggle();
+     $("#hidden1").toggle();
+   });
+
+ $(".development").click(function () {
+     $("#development").toggle();
+     $("#hidden2").toggle();
+   });
+ 
+ $(".product").click(function () {
+     $("#product").toggle();
+     $("#hidden3").toggle();
+   });
+});
+ 
